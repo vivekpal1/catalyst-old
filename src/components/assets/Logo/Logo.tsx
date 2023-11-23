3c721144eb14d7094e7a3dc222a712b827090d4e
vivekpal1 <31vivekpal@gmail.com>
2023-11-24 01:05:55 +0530

    asset: logo

---
+++ src/components/assets/Logo/Logo.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { cn } from '../../../lib/utils';
+
+export const Logo = React.forwardRef<
+  HTMLOrSVGElement,
+  React.HTMLAttributes<HTMLOrSVGElement>
+>(({ className, ...props }) => (
+<svg width="81" height="90" viewBox="0 0 81 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_bi_36_189)">
+<line x1="58.06" y1="39.4312" x2="71" y2="39.4312" stroke="#FF7A00" stroke-width="20" stroke-linecap="round"/>
+</g>
+<g filter="url(#filter1_bii_36_189)">
+<path d="M18.1243 24.8619C30.0827 11.5748 50.9173 11.5748 62.8757 24.8619V24.8619C73.1793 36.3104 73.1793 53.6896 62.8757 65.1381V65.1381C50.9173 78.4252 30.0827 78.4252 18.1243 65.1381V65.1381C7.82068 53.6896 7.82068 36.3104 18.1243 24.8619V24.8619Z" fill="white" fill-opacity="0.03"/>
+</g>
+<g filter="url(#filter2_bi_36_189)">
+<rect y="28.5312" width="15.66" height="10.725" rx="5.3625" fill="white" fill-opacity="0.1"/>
+</g>
+<g filter="url(#filter3_bi_36_189)">
+<rect x="0.809998" y="45.0312" width="41.85" height="11" rx="5.5" fill="white" fill-opacity="0.1"/>
+</g>
+<defs>
+<filter id="filter0_bi_36_189" x="-39.94" y="-58.5688" width="208.94" height="196" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feGaussianBlur in="BackgroundImageFix" stdDeviation="44"/>
+<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_36_189"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_36_189" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="2"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.841667 0 0 0 0 0.670807 0 0 0 0 0.547083 0 0 0 1 0"/>
+<feBlend mode="normal" in2="shape" result="effect2_innerShadow_36_189"/>
+</filter>
+<filter id="filter1_bii_36_189" x="-75.6034" y="-71.1034" width="232.207" height="232.207" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feGaussianBlur in="BackgroundImageFix" stdDeviation="43"/>
+<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_36_189"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_36_189" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.15 0"/>
+<feBlend mode="normal" in2="shape" result="effect2_innerShadow_36_189"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="34"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.05 0"/>
+<feBlend mode="normal" in2="effect2_innerShadow_36_189" result="effect3_innerShadow_36_189"/>
+</filter>
+<filter id="filter2_bi_36_189" x="-88" y="-59.4688" width="191.66" height="186.725" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feGaussianBlur in="BackgroundImageFix" stdDeviation="44"/>
+<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_36_189"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_36_189" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="2"/>
+<feGaussianBlur stdDeviation="1"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0"/>
+<feBlend mode="normal" in2="shape" result="effect2_innerShadow_36_189"/>
+</filter>
+<filter id="filter3_bi_36_189" x="-87.19" y="-42.9688" width="217.85" height="187" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feGaussianBlur in="BackgroundImageFix" stdDeviation="44"/>
+<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_36_189"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_36_189" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="2"/>
+<feGaussianBlur stdDeviation="1"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0"/>
+<feBlend mode="normal" in2="shape" result="effect2_innerShadow_36_189"/>
+</filter>
+</defs>
+</svg>
+));
+Logo.displayName = 'Logo';
